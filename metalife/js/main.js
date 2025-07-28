8b0a7940a52408c2f123ef1a31fa500aa1e77867
yuta <yuta.sw2305@gmail.com>
2025-07-29 00:40:00 +0900

    yutaのindexとjs追加

--- metalife/js/main.js
+++ metalife/js/main.js
@@ -1,3 +1,40 @@
+// 
+$(function () {
+  $(window).scroll(function () {
+    $(".left-content").each(function () {
+
+      var scroll = $(window).scrollTop();
+
+      var target = $(this).offset().top;
+
+      var windowHeight = $(window).height();
+
+      if (scroll > target - windowHeight + $(this).outerHeight()) {
+        // outerHeight()はpaddingを含めた高さを取得する
+        $(this).addClass("animation-left-reason");
+      }
+    });
+  
+    $(".right-content").each(function () {
+
+      var scroll = $(window).scrollTop();
+
+      var target = $(this).offset().top;
+
+      var windowHeight = $(window).height();
+
+      if (scroll > target - windowHeight + $(this).outerHeight()) {
+        // outerHeight()はpaddingを含めた高さを取得する
+        $(this).addClass("animation-right-reason");
+      }
+    });
+  
+  });
+});
+
+
+
+
 // アコーディオン
 $(function() {
   $(".faq dt").on("click", function () {
