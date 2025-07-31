793edc1b8cb0d289da836e19b518e13c3e93d70f
MASAKI <masa.sora01@gmail.com>
2025-07-31 23:29:17 +0900

    最新版

--- metalife/js/main.js
+++ metalife/js/main.js
@@ -32,7 +32,34 @@ $(function () {
   });
 });
 
+// キャラクター
+const follower = document.querySelector(".follower");
 
+let mouseX = window.innerWidth / 2;
+let mouseY = window.innerHeight / 2;
+let currentX = mouseX;
+let currentY = mouseY;
+
+// マウス位置更新
+document.addEventListener("mousemove", (e) => {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+});
+
+// 緩やかに追従
+function animate() {
+  // 線形補間 (lerp)
+  const speed = 0.02; // 数値が小さいほどゆっくり
+  currentX += (mouseX - currentX) * speed;
+  currentY += (mouseY - currentY) * speed;
+
+  follower.style.left = `${currentX}px`;
+  follower.style.top = `${currentY}px`;
+
+  requestAnimationFrame(animate);
+}
+
+animate();
 
 
 // アコーディオン
@@ -43,6 +70,19 @@ $(function() {
     
     // 他の答えを閉じる（任意）
     $(".faq dd").not($(this).next()).slideUp(300);
+  });
+});
+
+// アコーディオンのタイトルに「+」を追加
+$(function() {
+  $(".faq dt").each(function() {
+    $(this).append('<span class="toggle-icon">+</span>');
+  });
+
+  // タイトルクリック時の処理
+  $(".faq dt").on("click", function() {
+    $(this).toggleClass("is-active");
+    $(this).find(".toggle-icon").text($(this).hasClass("is-active") ? "−" : "+");
   });
 });
 
